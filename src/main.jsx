2a78875fe9bdb2554a19050bdf1049e48779bbb3
zudaardi-cell <zudaardi@gmail.com>
2026-02-01 12:13:26 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Microstock Genius</h1>
+    <App />
   </React.StrictMode>
-);
-
+)
